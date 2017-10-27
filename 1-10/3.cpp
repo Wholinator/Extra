83641941c69825d2f81d14e3419d4eb1482e826a
= <=>
2017-10-26 19:40:19 -0500

    First

---
+++ 1-10/3.cpp
@@ -0,0 +1,30 @@
+//
+// Created by S524063 on 10/25/2017.
+//
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long int n = 600851475143;
+    int last = 0;
+
+
+    for(long int i = 1; i < sqrt(n); i++) {
+        if (n % i == 0) {
+            bool prime = true;
+            for (int j = 1; j < sqrt(i); j++) {
+                if (j % i != 0) {
+                    prime = false;
+                }
+            }
+            if (prime) {
+                last = i;
+            }
+        }
+    } cout << last << endl;
+
+}
